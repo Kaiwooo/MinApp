de9bbe8db67cc43388b1f576f533ad06fa6fb245
p.gerasimenko <p.gerasimenko@medcall.center>
2026-02-24 22:54:29 +0300

    3

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import logging
 from fastapi import FastAPI
 from app.api.install import router as install_router
 from app.telegram.telegram_webhook import router as telegram_router
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 app = FastAPI()
 app.include_router(install_router)
 app.include_router(telegram_router)
